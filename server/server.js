a060b7f86eb0f9e4f8a76167b2e54400654c0c79
joe <joe@joeegan.co.uk>
2014-02-28 15:40:08 +0000

    login progress

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
 var restify = require('restify'),
-    server = restify.createServer();
+   client = restify.createClient({
+      url: 'https://web-api.ig.com/gateway/deal/session'
+   });
+server = restify.createServer();
 
 server.use(restify.fullResponse());
-server.use(restify.bodyParser());
+server.use(restify.bodyParser({mapParams: false}));
 server.use(restify.gzipResponse());
 server.listen(8080);
 
-/**
- * LOGIN
- *
- * Given a username and password, do the login and return sso token
- */
-server.post({
-    path: /^\/login/
-}, function(request, response, next) {
-
-    response.send('ssotokenhere');
-    return next();
+server.post('/login', function(req, res) {
+   console.log(req);
+   req.send(req);
+   client.post({ identifier: req.body.params.identifier, password: req.params.password}, function(err, response){
+      console.log(response);
+      res.send('hello ' + req.params.username, response);
+   });
 });
