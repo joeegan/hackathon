be761fee28c479a149d844e99a425f32edc889dc
Michael Bird <mike@b1rd.me>
2014-03-01 11:30:14 +0000

    login form render

--- client/www/js/mothership.js
+++ client/www/js/mothership.js
@@ -12,16 +12,75 @@ function init() {
       ev.preventDefault();
       data = $(this).serialize();
       $.post(SERVER_URL + '/login', data, function(details) {
-         CST = details['CST'];
-         SSO = details['X-SECURITY-TOKEN'];
+         $.ajaxSetup({
+            headers: {
+               'X-SECURITY-TOKEN': details['X-SECURITY-TOKEN'],
+               'CST': details['CST']
+            }
+         });
          render();
       }).fail(function(err) {
          alert(err);
       });
    });
+
+   $('#login').show();
+   $('#interface').hide();
 }
 $(document).ready(init);
 
+
+
 function render() {
-   alert(CST);
+
+   $('#login').hide();
+   $('#interface').show();
+
+   $.ajax({
+      url: 'http://localhost:8080/positions',
+      success: function(data) {
+         initChart(processData(data, 'quantity'))
+      }
+   });
+
+   function processData(data, key) {
+      data.forEach(function(position){
+         position.y = (position[key]/10)*100;
+      });
+      return data;
+   }
+
+   function initChart(data) {
+
+      $('#currently_trading').highcharts({
+         chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+         },
+         title: {
+            text: 'Markets in my Open Positions'
+         },
+         tooltip: {
+            pointFormat: '{series.name}: <b>{point.y.f}%</b>'
+         },
+         plotOptions: {
+            pie: {
+               allowPointSelect: true,
+               cursor: 'pointer',
+               dataLabels: {
+                  enabled: true,
+                  color: '#000000',
+                  connectorColor: '#000000',
+                  format: '<b>{point.name}</b>:{point.epic} {point.y:.1f} %'
+               }
+            }
+         },
+         series: [{
+            type: 'pie',
+            name: 'Open position',
+            data: data
+         }]
+      });
+   }
 }
