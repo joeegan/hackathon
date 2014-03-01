5a76e6867a5e54b310012faa27e50d86b007a550
jenglish81 <jimenglish81@gmail.com>
2014-03-01 16:23:25 +0000

    feed filter

--- client/www/js/mothership.js
+++ client/www/js/mothership.js
@@ -1,7 +1,16 @@
 var SERVER_URL = "http://localhost:8080",
    CLIENT_URL = "http://localhost:8000",
    CST,
-   SSO;
+   SSO,
+   suggestMarketsEpics,
+   suggestMarketsEpicsMap,
+   suggestMarketPoller,
+   currentlyTradingEpics,
+   currentlyTradingEpicsMap,
+   currentlyTradingPoller,
+   suggestMarketTimeout,
+   currentlyTradingTimeout,
+   selectedFeeds = [];
 
 function init() {
 
@@ -20,136 +29,142 @@ function init() {
          });
          render();
       }).fail(function(err) {
-         alert(err);
-      });
+            alert(err);
+         });
    });
 
    $('#login').show();
    $('#interface').hide();
-
-
 }
-var ajaxQueue = [];
 $(document).ready(init);
 
+function getMarketData(){
+   $.ajax({
+      url: 'http://localhost:8080/suggestedmarkets',
+   }).done(function(data) {
+         suggestMarketsEpics = data.map(function(market){
+            return market.epic;
+         }).join(',');
 
+         suggestMarketsEpicsMap = data.reduce(function(map, market){
+            map[market.epic] = market;
+            return map;
+         }, {});
+
+         $.ajax({
+            url: 'http://localhost:8080/currentlytrading'
+         }).done(function() {
+               currentlyTradingEpics = data.map(function(market){
+                  return market.epic;
+               }).join(',');
+
+               currentlyTradingEpicsMap = data.reduce(function(map, market){
+                  map[market.epic] = market;
+                  return map;
+               }, {});
+
+               currentlyTradingPoller = pollService.bind(null, currentlyTradingEpics, currentlyTradingEpicsMap, '#currently_trading', 'My Currently Trading Markets');
+               suggestMarketPoller = pollService.bind(null, suggestMarketsEpics, suggestMarketsEpicsMap, '#suggested_markets', 'Markets from my watchlists and recent history', function() {
+                  currentlyTradingTimeout = setTimeout(function() {
+                     currentlyTradingPoller();
+                  }, 1000);
+               });
+
+               suggestMarketPoller();
+            });
+      });
+}
+
+function pollService(epics, epicMap, elementId, title, callback) {
+   var args = arguments;
+   $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/everything/' + selectedFeeds.join(',') + '/' + epics,
+      success: function(scores) {
+         for (var epic in scores) {
+            epicMap[epic].index = scores[epic].index;
+            epicMap[epic].sentiment = scores[epic].sentiment;
+         }
+         if (callback) {
+            callback(scores, epicMap);
+         }
+         initChart(processData(epicMap, 'index'), elementId, title);
+         /*suggestMarketTimeout = setTimeout(function() {
+            pollService.apply(null, args);
+         }, 10000);*/
+      }
+   });
+}
+
+function processData(data, key) {
+   var total = 0;
+   for (market in data) {
+      var score = data[market][key];
+      total += score;
+   }
+   for (market in data) {
+      var score = data[market][key];
+      data[market].y = (score/total) * 100;
+   }
+   return $.map(data, function(item) {
+      return [item];
+   });
+}
+
+function initChart(data, selector, title) {
+
+   $(selector).highcharts({
+      chart: {
+         plotBackgroundColor: null,
+         plotBorderWidth: null,
+         plotShadow: false
+      },
+      title: {
+         text: title
+      },
+      tooltip: {
+         pointFormat: '<b>{point.y:.f}%</b>'
+      },
+      plotOptions: {
+         pie: {
+            animation: false,
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+               enabled: true,
+               color: '#000000',
+               connectorColor: '#000000',
+               format: '<b>{point.name}</b> {point.y:.1f} %'
+            }
+         }
+      },
+      series: [{
+         type: 'pie',
+         name: 'Open position',
+         data: data
+      }]
+   });
+}
 
 function render() {
 
    $('#login').hide();
    $('#interface').show();
+
    $('#feeds').on('click', 'input', function() {
-       alert('filterme:' + this.value);
-   });
-
-   function startCalls (){
-      ajaxQueue.push({
-         url: 'http://localhost:8080/suggestedmarkets',
-         success: function(data) {
-            getIndex(data, function(scores, epicsMap){
-               for (epic in scores) {
-                  epicsMap[epic].index = scores[epic].index;
-                  epicsMap[epic].sentiment = scores[epic].sentiment;
-               }
-               initChart(processData(epicsMap, 'index'), '#suggested_markets', 'Markets from my watchlists and recent history')
-            })
-         }
-      });
-
-      ajaxQueue.push({
-         url: 'http://localhost:8080/currentlytrading',
-         success: function(data) {
-            getIndex(data, function(scores, epicsMap){
-               for (epic in scores) {
-                  epicsMap[epic].index = scores[epic].index;
-                  epicsMap[epic].sentiment = scores[epic].sentiment;
-               }
-               initChart(processData(epicsMap, 'index'), '#currently_trading', 'My Currently Trading Markets')
-            })
-         }
-      });
-
-      setInterval(function(){
-         if (ajaxQueue.length) {
-            $.ajax(ajaxQueue.shift());
-         }
-      },500);
-
-   }
-
-   startCalls();
-
-   setTimeout(function(){
-      startCalls();
-   }, 10000);
-
-
-   function getIndex(data, callback){
-      var epics = data.map(function(market){
-         return market.epic;
-      }).join(',');
-      var epicsMap = {};
-      data.forEach(function(market){
-         epicsMap[market.epic] = market;
-      });
-
-      ajaxQueue.push({
-         type: 'GET',
-         url: 'http://localhost:8080/everything/sentiment/' + epics,
-         success: function(scores) {
-            callback(scores, epicsMap);
-         }
-      });
-
-   }
-
-   function processData(data, key) {
-      var total = 0;
-      for (market in data) {
-            var score = data[market][key];
-            total += score;
+      if ($(this).prop('checked')) {
+         selectedFeeds.push(this.value);
+      } else if (selectedFeeds.indexOf(this.value) > -1) {
+         selectedFeeds.splice(selectedFeeds.indexOf(this.value), 1);
       }
-      for (market in data) {
-         var score = data[market][key];
-         data[market].y = (score/total) * 100;
+      clearTimeout(suggestMarketTimeout);
+      clearTimeout(currentlyTradingTimeout);
+      if ($('#feeds :checked').length) {
+         suggestMarketPoller();
       }
-      return $.map(data, function(item) {
-         return [item];
+   }).find('input:checked').each(function() {
+         selectedFeeds.push(this.value);
       });
-   }
 
-   function initChart(data, selector, title) {
-
-      $(selector).highcharts({
-         chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-         },
-         title: {
-            text: title
-         },
-         tooltip: {
-            pointFormat: '<b>{point.y:.f}%</b>'
-         },
-         plotOptions: {
-            pie: {
-               animation: false,
-               allowPointSelect: true,
-               cursor: 'pointer',
-               dataLabels: {
-                  enabled: true,
-                  color: '#000000',
-                  connectorColor: '#000000',
-                  format: '<b>{point.name}</b> {point.y:.1f} %'
-               }
-            }
-         },
-         series: [{
-            type: 'pie',
-            name: 'Open position',
-            data: data
-         }]
-      });
-   }
+   getMarketData();
 }
